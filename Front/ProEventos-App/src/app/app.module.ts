52b727f801bffe9058ce77fce62ef9f7e5da27a1
vinipolitta <vinicius.politta1@gmail.com>
2022-05-10 05:34:44 -0300

    seçao 5 - angular - interpolação diretivas e binding

--- Front/ProEventos-App/src/app/app.module.ts
+++ Front/ProEventos-App/src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'
-
+import { FormsModule } from '@angular/forms';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { EventosComponent } from './components/eventos/eventos.component';
 import { PalestrantesComponent } from './components/palestrantes/palestrantes.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './components/nav/nav.component';
-import { CollapseModule } from 'ngx-bootstrap/collapse';
 
 @NgModule({
   declarations: [
@@ -22,6 +23,7 @@ import { CollapseModule } from 'ngx-bootstrap/collapse';
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
+    FormsModule,
     CollapseModule.forRoot()
   ],
   providers: [],
--- Front/ProEventos-App/src/app/components/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/components/eventos/eventos.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { EVENT_MANAGER_PLUGINS } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-eventos',
@@ -8,8 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
-  public URL = "https://localhost:5001/api/"
+  public eventos: any = [];
+  eventosFiltrados: any = [];
+  public URL = "https://localhost:5001/api/";
+  widthImg = 150;
+  marginImg = 2;
+  showImage = false;
+  private _filterList = '';
+
+  public get filterList(): string {
+    return this._filterList;
+  }
+
+  public set filterList(value: string) {
+    this._filterList = value;
+    this.eventosFiltrados = this.filterList ? this.filterEvents(this.filterList) : this.eventos;
+    console.log(this.filterList ? this.filterEvents(this.filterList) : this.eventos);
+
+  }
 
   constructor(private http: HttpClient)
   { }
@@ -18,12 +35,20 @@ export class EventosComponent implements OnInit {
     this.getEventos();
   }
 
-  public getEventos(): void {
-    this.http.get(`${this.URL +"Eventos"}`).subscribe(res =>
-      this.eventos = res,
-      error =>   console.log(error)
-
-    )
+  public filterEvents(filters: string): any {
+    filters = filters.toLocaleLowerCase();
+    return this.eventos.filter(
+      (event: any) => event.tema.toLocaleLowerCase().indexOf(filters) !== -1 ||
+      event.local.toLocaleLowerCase().indexOf(filters) !== -1
+    );
   }
 
+  public getEventos(): void {
+    this.http.get(`${this.URL +"Eventos"}`).subscribe(res =>{
+      this.eventos = res;
+      this.eventosFiltrados = this.eventos;
+    },
+      error =>   console.log(error)
+    );
+  }
 }
