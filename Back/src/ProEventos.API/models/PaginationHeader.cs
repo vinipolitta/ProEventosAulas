c4c2df15814d4d0548f72f514abeba43b2c6af75
Vinicius Politta <vinicius.politta1@gmail.com>
2022-10-27 16:31:40 -0300

    paginacao dinamica

--- Back/src/ProEventos.API/models/PaginationHeader.cs
+++ Back/src/ProEventos.API/models/PaginationHeader.cs
@@ -7,15 +7,15 @@ namespace ProEventos.API.models
 {
     public class PaginationHeader
     {
-        public PaginationHeader(int currentPage, int intemsPerPage, int totalItems, int totalPages)
+        public PaginationHeader(int currentPage, int itemsPerPage, int totalItems, int totalPages)
         {
             this.CurrentPage = currentPage;
-            this.IntemsPerPage = intemsPerPage;
+            this.itemsPerPage = itemsPerPage;
             this.TotalItems = totalItems;
             this.TotalPages = totalPages;
         }
         public int CurrentPage { get; set; }
-        public int IntemsPerPage { get; set; }
+        public int itemsPerPage { get; set; }
         public int TotalItems { get; set; }
         public int TotalPages { get; set; }
     }
